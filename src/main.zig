b396a2760554f4bc0578ec4f3826090534030ec0
stoyan-kukev <stoykukev@proton.me>
2024-12-02 10:40:01 +0200

    day 2 | star 2

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,30 @@ const std = @import("std");
 
 const input = @embedFile("input.txt");
 
+fn isSequenceSafe(values: []const i32) bool {
+    if (values.len <= 1) return false;
+
+    var is_increasing: ?bool = null;
+
+    for (1..values.len) |current| {
+        const diff = values[current] - values[current - 1];
+
+        if (@abs(diff) < 1 or @abs(diff) > 3) {
+            return false;
+        }
+
+        if (is_increasing) |increasing| {
+            if ((increasing and diff < 0) or (!increasing and diff > 0)) {
+                return false;
+            }
+        } else {
+            is_increasing = diff > 0;
+        }
+    }
+
+    return true;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -34,33 +58,32 @@ pub fn main() !void {
     var safe_levels: u32 = 0;
 
     for (levels.items) |values| {
-        if (values.len <= 1) continue;
+        if (isSequenceSafe(values)) {
+            safe_levels += 1;
+            continue;
+        }
 
-        var is_increasing: ?bool = null;
-        var is_safe = true;
+        var is_safe_dampened = false;
+        for (0..values.len) |skip_index| {
+            var test_values = std.ArrayList(i32).init(alloc);
+            defer test_values.deinit();
 
-        for (1..values.len) |current| {
-            const diff = values[current] - values[current - 1];
-
-            if (@abs(diff) < 1 or @abs(diff) > 3) {
-                is_safe = false;
-                break;
+            for (0..values.len) |i| {
+                if (i != skip_index) {
+                    try test_values.append(values[i]);
+                }
             }
 
-            if (is_increasing == null) {
-                is_increasing = diff > 0;
-            } else {
-                if ((is_increasing.? and diff < 0) or (!is_increasing.? and diff > 0)) {
-                    is_safe = false;
-                    break;
-                }
+            if (isSequenceSafe(test_values.items)) {
+                is_safe_dampened = true;
+                break;
             }
         }
 
-        if (is_safe) {
+        if (is_safe_dampened) {
             safe_levels += 1;
         }
     }
 
-    std.log.info("Safe levels: {}", .{safe_levels});
+    std.log.info("Safe levels with Problem Dampner: {}", .{safe_levels});
 }
