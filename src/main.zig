c6481456724a54eca451d5a79963a0f84e3a533e
stoyan-kukev <stoykukev@proton.me>
2024-12-06 20:48:25 +0200

    day 6 | star 2

--- src/main.zig
+++ src/main.zig
@@ -25,6 +25,10 @@ const Pos = struct {
             .y = self.y,
         };
     }
+
+    fn equalTo(self: Pos, other: Pos) bool {
+        return self.x == other.x and self.y == other.y;
+    }
 };
 
 const Dir = struct {
@@ -39,12 +43,7 @@ fn generateMapFromInput(allocator: std.mem.Allocator, input: []const u8) ![][]u8
     while (iter.next()) |line| {
         if (line.len < 1) continue;
 
-        var chars = std.ArrayList(u8).init(allocator);
-        for (line) |char| {
-            try chars.append(char);
-        }
-
-        try output.append(try chars.toOwnedSlice());
+        try output.append(try allocator.dupe(u8, line));
     }
 
     return try output.toOwnedSlice();
@@ -87,6 +86,9 @@ fn countGuardSteps(allocator: std.mem.Allocator, map: [][]u8, guard_pos: *Pos) !
     var visited_pos = std.AutoArrayHashMap(Pos, void).init(allocator);
     defer visited_pos.deinit();
 
+    var visited_blockades = std.AutoArrayHashMap(struct { pos: Pos, orientation: u8 }, void).init(allocator);
+    defer visited_blockades.deinit();
+
     while (true) {
         const current_guard = map[guard_pos.y][guard_pos.x];
         const guard_dir = try getGuardDirection(map, guard_pos.*);
@@ -100,8 +102,15 @@ fn countGuardSteps(allocator: std.mem.Allocator, map: [][]u8, guard_pos: *Pos) !
             return visited_pos.count();
         }
 
+        const state = .{ .pos = guard_pos.newFromSelf(), .orientation = current_guard };
+
+        if (visited_blockades.get(state)) |_| {
+            return error.LoopDetected;
+        }
+
         switch (map[new_guard_pos.y][new_guard_pos.x]) {
             '#' => {
+                try visited_blockades.put(state, {});
                 map[guard_pos.y][guard_pos.x] = getNewGuardRotation(current_guard);
             },
             '.' => {
@@ -127,6 +136,49 @@ fn printMap(map: [][]u8) void {
     }
 }
 
+fn deepCopyMap(allocator: std.mem.Allocator, map: [][]u8) ![][]u8 {
+    var output = try std.ArrayList([]u8).initCapacity(allocator, map.len);
+
+    for (map) |row| {
+        try output.append(try allocator.dupe(u8, row));
+    }
+
+    return try output.toOwnedSlice();
+}
+
+fn testObstaclePositions(allocator: std.mem.Allocator, map: [][]u8) !usize {
+    var obstacle_positions = std.ArrayList(Pos).init(allocator);
+    defer obstacle_positions.deinit();
+
+    for (map, 0..) |row, y| {
+        for (row, 0..) |cell, x| {
+            if (cell != '.') continue;
+
+            var map_copy = try deepCopyMap(allocator, map);
+            defer {
+                for (map_copy) |map_row| {
+                    allocator.free(map_row);
+                }
+                allocator.free(map_copy);
+            }
+
+            map_copy[y][x] = '#';
+
+            var guard_pos = try findGuardPos(map_copy);
+            const result = countGuardSteps(allocator, map_copy, &guard_pos);
+            if (result == error.LoopDetected) {
+                try obstacle_positions.append(.{ .x = x, .y = y });
+            }
+        }
+    }
+
+    return obstacle_positions.items.len;
+}
+
+/// NOTE: This current solution takes ~11 seconds to complete compiled on ReleaseSafe
+/// (that's to be expected, since it's a brute force solution), which is really bad
+/// FIXME: Think of a better algorithm in the future that doesn't take 11 seconds for
+/// a 100x100 grid (I think that's how big the input is, or close to it)
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -142,8 +194,6 @@ pub fn main() !void {
         allocator.free(map);
     }
 
-    var guard_pos = try findGuardPos(map);
-
-    const steps = try countGuardSteps(allocator, map, &guard_pos);
-    std.debug.print("Steps: {}\n", .{steps});
+    const obstacle_positions = try testObstaclePositions(allocator, map);
+    std.debug.print("Valid positions: {}\n", .{obstacle_positions});
 }
