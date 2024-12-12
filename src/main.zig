3c51bfb5154b9034881a9fb3344a71578400f79f
stoyan-kukev <stoykukev@proton.me>
2024-12-12 11:02:04 +0200

    day 9 | star 1

--- src/main.zig
+++ src/main.zig
@@ -1,70 +1,91 @@
 const std = @import("std");
 
-const Pos = struct {
-    x: isize,
-    y: isize,
+const DiskSector = struct {
+    const Self = @This();
 
-    fn distance(self: Pos, other: Pos) f64 {
-        const dx = self.x - other.x;
-        const dy = self.y - other.y;
-        return @sqrt(@as(f64, @floatFromInt(dx * dx + dy * dy)));
-    }
-
-    pub fn hash(self: Pos) u64 {
-        return @as(u64, @intCast(self.x)) * 31 + @as(u64, @intCast(self.y));
-    }
-
-    pub fn eql(self: Pos, other: Pos) bool {
-        return self.x == other.x and self.y == other.y;
-    }
+    id: ?usize,
+    count: usize,
 };
 
-const AntennaMap = std.AutoHashMap(u8, std.ArrayList(Pos));
+fn parseInput(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList(u32) {
+    var output = std.ArrayList(u32).init(allocator);
+    errdefer output.deinit();
 
-fn isValidAntinode(node: Pos, map: [][]const u8) bool {
-    return node.x >= 0 and node.y >= 0 and
-        @as(usize, @intCast(node.x)) < map[0].len and
-        @as(usize, @intCast(node.y)) < map.len;
-}
-
-fn findAntennas(allocator: std.mem.Allocator, map: [][]const u8) !AntennaMap {
-    var output = AntennaMap.init(allocator);
-    for (map, 0..) |row, j| {
-        for (row, 0..) |cell, i| {
-            const isDigit = cell >= '0' and cell <= '9';
-            const isLowerLetter = cell >= 'a' and cell <= 'z';
-            const isUpperLetter = cell >= 'A' and cell <= 'Z';
-            if (isDigit or isLowerLetter or isUpperLetter) {
-                if (output.getPtr(cell)) |entry| {
-                    try entry.append(.{ .x = @intCast(i), .y = @intCast(j) });
-                } else {
-                    var list = std.ArrayList(Pos).init(allocator);
-                    try list.append(.{ .x = @intCast(i), .y = @intCast(j) });
-                    try output.put(cell, list);
-                }
-            }
-        }
+    for (input) |char| {
+        if (!std.ascii.isDigit(char)) continue;
+        try output.append(try std.fmt.parseInt(u32, &.{char}, 10));
     }
+
     return output;
 }
 
-fn parseInput(allocator: std.mem.Allocator, input: []const u8) ![][]const u8 {
-    var output = std.ArrayList([]const u8).init(allocator);
-    defer output.deinit();
-    var iter = std.mem.splitSequence(u8, input, "\n");
-    while (iter.next()) |line| {
-        if (line.len > 0) {
-            try output.append(try allocator.dupe(u8, line));
+fn countSectors(allocator: std.mem.Allocator, items: []u32) !std.ArrayList(DiskSector) {
+    var sectors = std.ArrayList(DiskSector).init(allocator);
+
+    var id: usize = 0;
+    for (items, 0..) |num, i| {
+        if (i % 2 != 0) {
+            try sectors.append(.{ .id = null, .count = num });
+        } else {
+            try sectors.append(.{ .id = id, .count = num });
+            id += 1;
         }
     }
-    return try output.toOwnedSlice();
+
+    return sectors;
 }
 
-fn calculateAntinode(pos1: Pos, pos2: Pos, i: isize) Pos {
-    return .{
-        .x = pos1.x + i * (pos2.x - pos1.x),
-        .y = pos1.y + i * (pos2.y - pos1.y),
-    };
+pub fn reorderSectors(sectors: *std.ArrayList(DiskSector)) !usize {
+    var i: usize = 0;
+    var j: usize = sectors.items.len - 1;
+
+    while (i != j + 1) {
+        if (sectors.items[j].id == null) {
+            j -= 1;
+            continue;
+        }
+
+        if (sectors.items[i].id != null) {
+            i += 1;
+            continue;
+        }
+
+        if (sectors.items[i].count > sectors.items[j].count) {
+            sectors.items[i].count -= sectors.items[j].count;
+
+            try sectors.insert(i, .{
+                .id = sectors.items[j].id,
+                .count = sectors.items[j].count,
+            });
+
+            i += 1;
+            j -= 1;
+        } else if (sectors.items[i].count == sectors.items[j].count) {
+            sectors.items[i] = .{
+                .id = sectors.items[j].id,
+                .count = sectors.items[j].count,
+            };
+
+            sectors.items[j] = .{
+                .id = null,
+                .count = sectors.items[j].count,
+            };
+
+            i += 1;
+            j -= 1;
+        } else {
+            sectors.items[j].count -= sectors.items[i].count;
+
+            sectors.items[i] = .{
+                .id = sectors.items[j].id,
+                .count = sectors.items[i].count,
+            };
+
+            i += 1;
+        }
+    }
+
+    return i;
 }
 
 pub fn main() !void {
@@ -73,55 +94,24 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     const input = @embedFile("input.txt");
+
     const map = try parseInput(allocator, input);
-    defer {
-        for (map) |row| {
-            allocator.free(row);
-        }
-        allocator.free(map);
-    }
+    defer map.deinit();
 
-    var antenna_map = try findAntennas(allocator, map);
-    defer {
-        var iter = antenna_map.valueIterator();
-        while (iter.next()) |list| {
-            list.deinit();
-        }
-        antenna_map.deinit();
-    }
+    var sectors = try countSectors(allocator, map.items);
+    defer sectors.deinit();
 
-    var antinode_positions = std.AutoHashMap(Pos, void).init(allocator);
-    defer antinode_positions.deinit();
+    const midpoint = try reorderSectors(&sectors);
 
-    var freq_iter = antenna_map.iterator();
-    while (freq_iter.next()) |freq_entry| {
-        const positions = freq_entry.value_ptr.*;
-
-        for (0..positions.items.len) |i| {
-            for (i + 1..positions.items.len) |j| {
-                const ant_pos1 = positions.items[i];
-                const ant_pos2 = positions.items[j];
-
-                var k: isize = 1;
-                var l: isize = 1;
-
-                // Calculate forward and backward antinodes
-                var possible_node1 = calculateAntinode(ant_pos1, ant_pos2, k);
-                var possible_node2 = calculateAntinode(ant_pos2, ant_pos1, l);
-
-                while (isValidAntinode(possible_node1, map)) : (k += 1) {
-                    try antinode_positions.put(possible_node1, {});
-                    possible_node1 = calculateAntinode(ant_pos1, ant_pos2, k);
-                }
-
-                while (isValidAntinode(possible_node2, map)) : (l += 1) {
-                    try antinode_positions.put(possible_node2, {});
-                    possible_node2 = calculateAntinode(ant_pos2, ant_pos1, l);
-                }
-            }
+    var checksum: usize = 0;
+    var id: usize = 0;
+    for (0..midpoint) |i| {
+        while (sectors.items[i].count != 0) {
+            checksum += sectors.items[i].id.? * id;
+            sectors.items[i].count -= 1;
+            id += 1;
         }
     }
 
-    // Print the count of unique antinode positions
-    std.debug.print("Number of unique antinode positions: {}\n", .{antinode_positions.count()});
+    std.debug.print("Checksum: {}\n", .{checksum});
 }
