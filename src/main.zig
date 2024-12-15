70ecec9a8035cb6fd5288919bcaa22c842d347dd
stoyan-kukev <stoykukev@proton.me>
2024-12-15 11:14:47 +0200

    day 9 | star 2

--- src/main.zig
+++ src/main.zig
@@ -35,57 +35,73 @@ fn countSectors(allocator: std.mem.Allocator, items: []u32) !std.ArrayList(DiskS
     return sectors;
 }
 
-pub fn reorderSectors(sectors: *std.ArrayList(DiskSector)) !usize {
-    var i: usize = 0;
-    var j: usize = sectors.items.len - 1;
-
-    while (i != j + 1) {
-        if (sectors.items[j].id == null) {
-            j -= 1;
-            continue;
-        }
-
-        if (sectors.items[i].id != null) {
-            i += 1;
-            continue;
-        }
-
-        if (sectors.items[i].count > sectors.items[j].count) {
-            sectors.items[i].count -= sectors.items[j].count;
-
-            try sectors.insert(i, .{
-                .id = sectors.items[j].id,
-                .count = sectors.items[j].count,
-            });
-
-            i += 1;
-            j -= 1;
-        } else if (sectors.items[i].count == sectors.items[j].count) {
-            sectors.items[i] = .{
-                .id = sectors.items[j].id,
-                .count = sectors.items[j].count,
-            };
-
-            sectors.items[j] = .{
-                .id = null,
-                .count = sectors.items[j].count,
-            };
-
-            i += 1;
-            j -= 1;
-        } else {
-            sectors.items[j].count -= sectors.items[i].count;
-
-            sectors.items[i] = .{
-                .id = sectors.items[j].id,
-                .count = sectors.items[i].count,
-            };
-
-            i += 1;
+fn findSectorById(sectors: *std.ArrayList(DiskSector), id: usize) !usize {
+    for (sectors.items, 0..) |sector, i| {
+        if (sector.id) |sec_id| {
+            if (sec_id == id) {
+                return i;
+            }
         }
     }
 
-    return i;
+    return error.CantFindSector;
+}
+
+pub fn reorderSectors(sectors: *std.ArrayList(DiskSector)) !void {
+    var curr_id_to_check: usize = blk: {
+        var max_id: usize = 0;
+        for (sectors.items) |sector| {
+            max_id = @max(max_id, sector.id orelse 0);
+        }
+        break :blk max_id;
+    };
+
+    while (curr_id_to_check != 0) : (curr_id_to_check -= 1) {
+        var free_space_idx: usize = 0;
+        const sec_to_move = try findSectorById(sectors, curr_id_to_check);
+
+        while (free_space_idx < sec_to_move) : (free_space_idx += 1) {
+            if (sectors.items[free_space_idx].id != null) continue;
+
+            if (sectors.items[free_space_idx].count == sectors.items[sec_to_move].count) {
+                sectors.items[free_space_idx].id = sectors.items[sec_to_move].id;
+                sectors.items[sec_to_move].id = null;
+
+                break;
+            } else if (sectors.items[free_space_idx].count > sectors.items[sec_to_move].count) {
+                sectors.items[free_space_idx].count -= sectors.items[sec_to_move].count;
+                const sector_copy = sectors.items[sec_to_move];
+
+                sectors.items[sec_to_move].id = null;
+                try sectors.insert(free_space_idx, .{
+                    .id = sector_copy.id,
+                    .count = sector_copy.count,
+                });
+
+                break;
+            }
+        }
+    }
+}
+
+fn mergeFreeSpaces(sectors: *std.ArrayList(DiskSector)) !void {
+    var i: usize = 0;
+    while (i < sectors.items.len) {
+        if (sectors.items[i].id == null) {
+            var acc: usize = sectors.items[i].count;
+            const j: usize = i + 1;
+
+            while (j < sectors.items.len and sectors.items[j].id == null) {
+                acc += sectors.items[j].count;
+                _ = sectors.orderedRemove(j);
+            }
+
+            if (acc > sectors.items[i].count) {
+                sectors.items[i].count = acc;
+            }
+        }
+        i += 1;
+    }
 }
 
 pub fn main() !void {
@@ -101,15 +117,21 @@ pub fn main() !void {
     var sectors = try countSectors(allocator, map.items);
     defer sectors.deinit();
 
-    const midpoint = try reorderSectors(&sectors);
+    try reorderSectors(&sectors);
+    try mergeFreeSpaces(&sectors);
 
     var checksum: usize = 0;
-    var id: usize = 0;
-    for (0..midpoint) |i| {
-        while (sectors.items[i].count != 0) {
-            checksum += sectors.items[i].id.? * id;
-            sectors.items[i].count -= 1;
-            id += 1;
+    var pos: usize = 0;
+    for (sectors.items) |*sector| {
+        if (sector.id == null) {
+            pos += sector.count;
+            continue;
+        }
+
+        while (sector.count > 0) {
+            sector.count -= 1;
+            checksum += sector.id.? * pos;
+            pos += 1;
         }
     }
 
