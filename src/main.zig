0107ad31cbfe70bc1a54f4942e7ed7365b2cea1c
stoyan-kukev <stoykukev@proton.me>
2024-12-18 10:14:50 +0200

    day 14 | star 2

--- src/main.zig
+++ src/main.zig
@@ -9,22 +9,6 @@ const Vec = struct {
 const Robot = struct {
     pos: Vec,
     dir: Vec,
-
-    pub fn format(
-        self: Robot,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = fmt;
-        _ = options;
-
-        try writer.print("Pos: ({}, {}) Vec: ({}, {}", .{
-            self.pos.x, self.pos.y, self.dir.x, self.dir.y,
-        });
-
-        try writer.writeAll(")");
-    }
 };
 
 const map_bounds: Vec = .{
@@ -44,6 +28,65 @@ fn parseVec(str: []const u8) !Vec {
     };
 }
 
+fn solve(allocator: std.mem.Allocator, input: []const u8) !usize {
+    var robots = try parseInput(allocator, input);
+    defer robots.deinit();
+
+    var min_entropy: usize = std.math.maxInt(usize);
+    var best_iteration: usize = 0;
+
+    for (0..@as(usize, @intCast(map_bounds.x * map_bounds.y))) |step| {
+        var result = std.ArrayList(Vec).init(allocator);
+        defer result.deinit();
+
+        var current_robots = std.ArrayList(Robot).init(allocator);
+        defer current_robots.deinit();
+        for (robots.items) |robot| {
+            try current_robots.append(Robot{
+                .pos = .{
+                    .x = @mod(robot.pos.x + robot.dir.x * @as(isize, @intCast(step)), map_bounds.x),
+                    .y = @mod(robot.pos.y + robot.dir.y * @as(isize, @intCast(step)), map_bounds.y),
+                },
+                .dir = robot.dir,
+            });
+        }
+
+        const ver_mid: isize = @divFloor(map_bounds.y - 1, 2);
+        const hor_mid: isize = @divFloor(map_bounds.x - 1, 2);
+        var q1: usize = 0;
+        var q2: usize = 0;
+        var q3: usize = 0;
+        var q4: usize = 0;
+
+        for (current_robots.items) |robot| {
+            if (robot.pos.x == hor_mid or robot.pos.y == ver_mid) continue;
+
+            if (robot.pos.x < hor_mid) {
+                if (robot.pos.y < ver_mid) {
+                    q1 += 1;
+                } else {
+                    q3 += 1;
+                }
+            } else {
+                if (robot.pos.y < ver_mid) {
+                    q2 += 1;
+                } else {
+                    q4 += 1;
+                }
+            }
+        }
+
+        const entropy = q1 * q3 * q2 * q4;
+
+        if (entropy < min_entropy) {
+            min_entropy = entropy;
+            best_iteration = step;
+        }
+    }
+
+    return best_iteration;
+}
+
 fn parseInput(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList(Robot) {
     var output = std.ArrayList(Robot).init(allocator);
 
@@ -61,64 +104,12 @@ fn parseInput(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList(Ro
     return output;
 }
 
-fn checkMiddleOfBounds(pos: Vec) bool {
-    const middleX = map_bounds.x / 2;
-    const middleY = map_bounds.y / 2;
-
-    const xIsOutsideMiddle =
-        if (map_bounds.x % 2 == 0)
-        pos.x < middleX or pos.x >= middleX + 2
-    else
-        pos.x != middleX;
-
-    const yIsOutsideMiddle =
-        if (map_bounds.y % 2 == 0)
-        pos.y < middleY or pos.y >= middleY + 2
-    else
-        pos.y != middleY;
-
-    return xIsOutsideMiddle and yIsOutsideMiddle;
-}
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
     const input = @embedFile("input.txt");
-    const robots = try parseInput(allocator, input);
-    defer robots.deinit();
-
-    for (robots.items) |*robot| {
-        robot.pos.x += robot.dir.x * 100;
-        robot.pos.y += robot.dir.y * 100;
-
-        robot.pos.x = @mod(robot.pos.x, map_bounds.x);
-        robot.pos.y = @mod(robot.pos.y, map_bounds.y);
-    }
-
-    var q1: usize = 0;
-    var q2: usize = 0;
-    var q3: usize = 0;
-    var q4: usize = 0;
-
-    for (robots.items) |robot| {
-        if (!checkMiddleOfBounds(robot.pos)) continue;
-
-        const isLeft = robot.pos.x < map_bounds.x / 2;
-        const isTop = robot.pos.y < map_bounds.y / 2;
-
-        if (isLeft and isTop) {
-            q1 += 1;
-        } else if (!isLeft and isTop) {
-            q2 += 1;
-        } else if (isLeft and !isTop) {
-            q3 += 1;
-        } else {
-            q4 += 1;
-        }
-    }
-    const safety_factor = q1 * q2 * q3 * q4;
-
-    print("Safety factor: {}\n", .{safety_factor});
+    const result = try solve(allocator, input);
+    print("{}\n", .{result});
 }
