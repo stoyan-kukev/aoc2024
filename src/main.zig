2e510215b59498fc8c0a946faa65c4d10ddc60d0
stoyan-kukev <stoykukev@proton.me>
2024-12-22 12:49:13 +0200

    day 20 | star 1

--- src/main.zig
+++ src/main.zig
@@ -1,50 +1,63 @@
 const std = @import("std");
 const print = std.debug.print;
 
-fn parsePatterns(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList([]u8) {
+fn parseInput(allocator: std.mem.Allocator, input: []const u8) ![][]u8 {
     var output = std.ArrayList([]u8).init(allocator);
 
-    var input_iter = std.mem.splitSequence(u8, std.mem.trim(u8, input, "\n"), ", ");
-    while (input_iter.next()) |pattern| {
-        try output.append(try allocator.dupe(u8, pattern));
-    }
-
-    return output;
-}
-
-fn parseDesigns(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList([]u8) {
-    var output = std.ArrayList([]u8).init(allocator);
-
-    var input_iter = std.mem.splitSequence(u8, input, "\n");
-    while (input_iter.next()) |line| {
+    var iter = std.mem.splitSequence(u8, input, "\n");
+    while (iter.next()) |line| {
         if (line.len < 1) continue;
 
         try output.append(try allocator.dupe(u8, line));
     }
 
-    return output;
+    return try output.toOwnedSlice();
 }
 
-fn validPattern(design: []u8, patterns: [][]u8, cache: *std.StringHashMap(usize)) !usize {
-    if (design.len == 0) {
-        return 1;
-    }
+const Pos = struct {
+    row: isize,
+    col: isize,
+};
 
-    if (cache.get(design)) |val| {
-        return val;
-    }
+const State = struct {
+    pos: Pos,
+    steps: isize,
+};
 
-    var count: usize = 0;
+const DIRECTIONS = [4]Pos{
+    .{ .row = 0, .col = 1 },
+    .{ .row = 0, .col = -1 },
+    .{ .row = 1, .col = 0 },
+    .{ .row = -1, .col = 0 },
+};
 
-    for (patterns) |pattern| {
-        if (std.mem.startsWith(u8, design, pattern)) {
-            count += try validPattern(design[pattern.len..], patterns, cache);
+fn generateDists(allocator: std.mem.Allocator, grid: [][]u8, start_pos: Pos) !std.AutoHashMap(Pos, isize) {
+    var output = std.AutoHashMap(Pos, isize).init(allocator);
+
+    var queue = std.ArrayList(State).init(allocator);
+    defer queue.deinit();
+
+    try queue.append(.{ .pos = start_pos, .steps = 0 });
+
+    while (queue.items.len > 0) {
+        const state = queue.orderedRemove(0);
+
+        if (output.get(state.pos) != null) {
+            continue;
+        }
+
+        try output.put(state.pos, state.steps);
+        for (DIRECTIONS) |dir| {
+            const new_row = state.pos.row + dir.row;
+            const new_col = state.pos.col + dir.col;
+
+            if (grid[@intCast(new_col)][@intCast(new_row)] != '#') {
+                try queue.append(.{ .pos = .{ .row = new_row, .col = new_col }, .steps = state.steps + 1 });
+            }
         }
     }
 
-    try cache.put(design, count);
-
-    return count;
+    return output;
 }
 
 pub fn main() !void {
@@ -54,33 +67,81 @@ pub fn main() !void {
 
     const input = @embedFile("input.txt");
 
-    var section_iter = std.mem.splitSequence(u8, input, "\n\n");
-    const pattern_section = section_iter.next().?;
-    const designs_section = section_iter.next().?;
-
-    const patterns = try parsePatterns(allocator, pattern_section);
+    const grid = try parseInput(allocator, input);
     defer {
-        for (patterns.items) |pattern| {
-            allocator.free(pattern);
+        for (grid) |row| {
+            allocator.free(row);
         }
-        patterns.deinit();
+        allocator.free(grid);
     }
 
-    const designs = try parseDesigns(allocator, designs_section);
-    defer {
-        for (designs.items) |design| {
-            allocator.free(design);
+    const start_pos: Pos = blk: {
+        for (grid, 0..) |row, y| {
+            for (row, 0..) |char, x| {
+                if (char == 'S') {
+                    break :blk .{ .row = @intCast(x), .col = @intCast(y) };
+                }
+            }
         }
-        designs.deinit();
+
+        unreachable;
+    };
+
+    var dists = try generateDists(allocator, grid, start_pos);
+    defer dists.deinit();
+
+    var shortcuts = std.AutoHashMap(isize, usize).init(allocator);
+    defer shortcuts.deinit();
+
+    const allowed_timeskip = 2;
+    const target_time_to_save = 100;
+
+    for (0..grid.len) |col| {
+        for (0..grid[0].len) |row| {
+            const curr_pos = Pos{ .row = @intCast(row), .col = @intCast(col) };
+            const curr_steps = dists.get(curr_pos);
+            if (curr_steps == null) continue;
+
+            for (2..allowed_timeskip + 1) |len_cheat| {
+                for (0..len_cheat + 1) |dr| {
+                    const dc: isize = @intCast(len_cheat - dr);
+
+                    var new_positions = std.AutoHashMap(Pos, void).init(allocator);
+                    defer new_positions.deinit();
+
+                    const r: isize = @intCast(row);
+                    const c: isize = @intCast(col);
+                    const dr_i: isize = @intCast(dr);
+
+                    try new_positions.put(.{ .row = r + dr_i, .col = c + dc }, {});
+                    try new_positions.put(.{ .row = r + dr_i, .col = c - dc }, {});
+                    try new_positions.put(.{ .row = r - dr_i, .col = c + dc }, {});
+                    try new_positions.put(.{ .row = r - dr_i, .col = c - dc }, {});
+
+                    var iter = new_positions.keyIterator();
+                    while (iter.next()) |new_pos| {
+                        const new_steps = dists.get(new_pos.*);
+                        if (new_steps == null) continue;
+
+                        const time_saved = new_steps.? - curr_steps.? - @as(isize, @intCast(len_cheat));
+                        if (time_saved >= target_time_to_save) {
+                            if (shortcuts.get(time_saved)) |prev_count| {
+                                try shortcuts.put(time_saved, prev_count + 1);
+                            } else {
+                                try shortcuts.put(time_saved, 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    var cache = std.StringHashMap(usize).init(allocator);
-    defer cache.deinit();
-
-    var possible_designs: usize = 0;
-    for (designs.items) |design| {
-        possible_designs += try validPattern(design, patterns.items, &cache);
+    var sum: usize = 0;
+    var iter = shortcuts.iterator();
+    while (iter.next()) |entry| {
+        sum += entry.value_ptr.*;
     }
 
-    print("Possible designs: {}\n", .{possible_designs});
+    print("Cheats that save above {} for {} timeskip -> {}\n", .{ target_time_to_save, allowed_timeskip, sum });
 }
