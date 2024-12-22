cb6977e8035d6d37ddc5156fea34d256fdc093f6
stoyan-kukev <stoykukev@proton.me>
2024-12-22 11:04:17 +0200

    day 19 | star 1

--- src/main.zig
+++ src/main.zig
@@ -1,87 +1,44 @@
 const std = @import("std");
 const print = std.debug.print;
 
-const width = 71;
-const height = 71;
+fn parsePatterns(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList([]u8) {
+    var output = std.ArrayList([]u8).init(allocator);
 
-const Pos = struct {
-    row: isize,
-    col: isize,
-};
-
-fn parseInput(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList(Pos) {
-    var output = std.ArrayList(Pos).init(allocator);
-
-    var input_iter = std.mem.splitSequence(u8, input, "\n");
-    while (input_iter.next()) |line| {
-        if (line.len < 1) continue;
-
-        var num_iter = std.mem.splitSequence(u8, line, ",");
-        const row = try std.fmt.parseInt(isize, num_iter.next().?, 10);
-        const col = try std.fmt.parseInt(isize, num_iter.next().?, 10);
-
-        try output.append(.{ .row = row, .col = col });
+    var input_iter = std.mem.splitSequence(u8, std.mem.trim(u8, input, "\n"), ", ");
+    while (input_iter.next()) |pattern| {
+        try output.append(try allocator.dupe(u8, pattern));
     }
 
     return output;
 }
 
-const DIRECTIONS = [4]Pos{
-    .{ .row = 1, .col = 0 },
-    .{ .row = 0, .col = 1 },
-    .{ .row = -1, .col = 0 },
-    .{ .row = 0, .col = -1 },
-};
+fn parseDesigns(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList([]u8) {
+    var output = std.ArrayList([]u8).init(allocator);
 
-const State = struct {
-    pos: Pos,
-    steps: usize,
-};
+    var input_iter = std.mem.splitSequence(u8, input, "\n");
+    while (input_iter.next()) |line| {
+        if (line.len < 1) continue;
 
-fn findPath(allocator: std.mem.Allocator, blocks: []Pos, progress: usize) !usize {
-    var queue = std.ArrayList(State).init(allocator);
-    defer queue.deinit();
+        try output.append(try allocator.dupe(u8, line));
+    }
 
-    try queue.append(.{ .pos = .{ .row = 0, .col = 0 }, .steps = 0 });
+    return output;
+}
 
-    var visited = std.AutoHashMap(Pos, void).init(allocator);
-    defer visited.deinit();
+fn validPattern(design: []u8, patterns: [][]u8) bool {
+    if (design.len == 0) {
+        return true;
+    }
 
-    while (queue.items.len > 0) {
-        const state = queue.orderedRemove(0);
-
-        if (visited.get(.{ .row = state.pos.row, .col = state.pos.col }) != null) {
-            continue;
-        }
-
-        try visited.put(.{ .row = state.pos.row, .col = state.pos.col }, {});
-
-        if (state.pos.row == width - 1 and state.pos.col == height - 1) {
-            return state.steps;
-        }
-
-        for (DIRECTIONS) |dir| {
-            const new_row = state.pos.row + dir.row;
-            const new_col = state.pos.col + dir.col;
-
-            const is_in_bounds = new_row >= 0 and new_row < width and new_col >= 0 and new_col < height;
-            const is_blockade = blk: {
-                for (blocks[0..progress]) |item| {
-                    if (item.row == new_row and item.col == new_col) {
-                        break :blk true;
-                    }
-                }
-
-                break :blk false;
-            };
-
-            if (is_in_bounds and !is_blockade) {
-                try queue.append(.{ .pos = .{ .row = new_row, .col = new_col }, .steps = state.steps + 1 });
+    for (patterns) |pattern| {
+        if (std.mem.startsWith(u8, design, pattern)) {
+            if (validPattern(design[pattern.len..], patterns)) {
+                return true;
             }
         }
     }
 
-    return error.NoPathFound;
+    return false;
 }
 
 pub fn main() !void {
@@ -91,20 +48,32 @@ pub fn main() !void {
 
     const input = @embedFile("input.txt");
 
-    const blocks = try parseInput(allocator, input);
-    defer blocks.deinit();
+    var section_iter = std.mem.splitSequence(u8, input, "\n\n");
+    const pattern_section = section_iter.next().?;
+    const designs_section = section_iter.next().?;
 
-    var low: usize = 0;
-    var high: usize = blocks.items.len - 1;
+    const patterns = try parsePatterns(allocator, pattern_section);
+    defer {
+        for (patterns.items) |pattern| {
+            allocator.free(pattern);
+        }
+        patterns.deinit();
+    }
 
-    while (low != high) {
-        const mid = @divFloor(low + high, 2);
-        if (findPath(allocator, blocks.items, mid + 1) == error.NoPathFound) {
-            high = mid;
-        } else {
-            low = mid + 1;
+    const designs = try parseDesigns(allocator, designs_section);
+    defer {
+        for (designs.items) |design| {
+            allocator.free(design);
+        }
+        designs.deinit();
+    }
+
+    var valid_designs: usize = 0;
+    for (designs.items) |design| {
+        if (validPattern(design, patterns.items)) {
+            valid_designs += 1;
         }
     }
 
-    print("Answer pos: {any}\n", .{blocks.items[low]});
+    print("Valid designs: {}\n", .{valid_designs});
 }
